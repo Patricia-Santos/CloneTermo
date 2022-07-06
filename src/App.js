91db882dc002f4ebc0d5e6e937b0b3eca89b3c82
Patricia-Santos <santos.patricia@academico.ifpb.edu.br>
2022-07-05 21:06:38 -0300

    Adicionando teclado

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import { useState, createContext } from 'react';
+import { boardDefault } from './Words';
 import './App.css';
 import Board from './components/Board';
 import Keyboard from './components/Keyboard';
-import { boardDefault } from './Words';
 
 export const AppContext = createContext();
 
-function App() {
+function App(keyValue) {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0});
+  
+  const onSelectLetter = (keyValue) => {
+    if (currAttempt.letterPos > 4) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos] = keyValue;
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos + 1});
+  } 
+
+  const onDelete = () => {
+    if (currAttempt.letterPos === 0) return;
+    const newBoard = [...board];
+    newBoard[currAttempt.attempt][currAttempt.letterPos -1] = "";
+    setBoard(newBoard);
+    setCurrAttempt({...currAttempt, letterPos: currAttempt.letterPos -1});
+  }
+
+  const onEnter = () => {
+    if (currAttempt.letterPos !== 5) return;
+    setCurrAttempt({attempt: currAttempt.attempt + 1, letterPos: 0});
+  }
+
   return (
     <div className="App">
       <nav>
         <h1>TERMO</h1>
       </nav>
-      <AppContext.Provider value={{board, setBoard}}>
-        <Board />
-        <Keyboard />
+      <AppContext.Provider value={{board, setBoard, currAttempt, setCurrAttempt, onSelectLetter, onDelete, onEnter}}>
+        <div className='game'>
+          <Board />
+          <Keyboard />
+        </div>  
       </AppContext.Provider>
     </div>
   );
---
+++ src/components/Key.js
@@ -0,0 +1,25 @@
+import React, { useContext } from "react";
+import { AppContext } from '../App';
+
+function Key({ keyValue, bigKey }) {
+    const { onSelectLetter, onDelete, onEnter } = useContext(AppContext);
+
+    const selectLetter = () => {
+        if (keyValue === "ENTER"){
+            onEnter();
+        }
+        else if (keyValue === "APAGAR"){
+            onDelete();
+        }
+        else {
+            onSelectLetter(keyValue);
+            }
+        }
+    return(
+        <div className="key" id={bigKey && "big"} onClick={selectLetter}>
+            {keyValue}
+        </div>
+    )
+}
+
+export default Key
